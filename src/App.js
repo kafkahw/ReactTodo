a5fb9c658637d174d97eeb08d2a6be2d7fb0ef38
Wei Hou <wei.hou@cerner.com>
2017-01-30 21:27:46 -0600

    Initial Todo app

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        {id: 1, name: 'Learn JSX', isComplete: false},
+        {id: 2, name: 'Build an Awesome App', isComplete: false},
+        {id: 3, name: 'Ship It', isComplete: false}
+      ],
+      currentTodo: '',
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({
+      currentTodo: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Todos</h2>
+        </div>
+        <div className="Todo-App">
+          <form>
+            <input
+              type="text"
+              onChange={this.handleInputChange}
+              value={this.state.currentTodo} />
+          </form>
+          <div className="Todo-list">
+            <ul>
+              {this.state.todos.map(todo =>
+                <li key={todo.id}>
+                  <input type="checkbox" defaultChecked={todo.isComplete}/>
+                  {todo.name}
+                </li>)}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
